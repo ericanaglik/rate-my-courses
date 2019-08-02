9a5e5787d19215663c5b5747496ea5a12ead30e8
erica2605@live.com <erica2605@live.com>
2019-08-02 14:45:07 -0700

    beautified code to convention

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,17 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './NavBar.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
 
-const NavBar = (props) => {
+const NavBar = props => {
   return (
     <div>
       <ul>
-        <Link to="/" className="navbar__link"><li>Home</li></Link>
-        <Link to="/login" className="navbar__link"><li>Login</li></Link>
-        <Link to="/signup" className="navbar__link"><li>Sign Up</li></Link>
-        </ul>
+        <Link to="/" className="navbar__link">
+          <li>Home</li>
+        </Link>
+        <Link to="/login" className="navbar__link">
+          <li>Login</li>
+        </Link>
+        <Link to="/signup" className="navbar__link">
+          <li>Sign Up</li>
+        </Link>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
