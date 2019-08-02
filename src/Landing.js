88c37434c787fb68a5f1c384941ae3e36d8c5fe9
erica2605@live.com <erica2605@live.com>
2019-08-01 17:43:18 -0700

    make school logo positioned good

--- src/Landing.js
+++ src/Landing.js
@@ -12,9 +12,11 @@ function Landing(props) {
         <h1>Courses</h1>
         <h1>Told By Students</h1>
         </div>
+        
         <div class="pls">
         <button class="button button4">VIEW COURSES</button>
         <div class="landing"><img src={landingpic} alt="" /></div>
+        <img src="https://res.cloudinary.com/erica-naglik/image/upload/v1564602895/Screen_Shot_2019-06-07_at_7.46.14_PM_cbfogy.png" alt="" className="ms" />
         </div>
         </div>
         )
